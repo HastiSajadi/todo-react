c4470b4a2d33c5acbb19d7223e81c7f7bc150b6e
hasti sajadi <hastiisajadii@gmail.com>
2023-11-22 21:10:35 +0330

    get the number of task list and completed tasks

--- todo-app/src/components/task/index.jsx
+++ todo-app/src/components/task/index.jsx
@@ -2,17 +2,18 @@ import { TaskList } from '../task-list'
 import styles from './task.module.css'
 
 export function Task({tasks}){
-    
+    const tasksQuantity = tasks.length;
+    const completedTasks = tasks.filter(task => task.isCompleted).length;
     return(
         <section className={styles.task}>
             <header className={styles.header}>
                 <div>
                     <p> Create Tasks</p>
-                    <span>10</span>
+                    <span>{tasksQuantity}</span>
                 </div>
                 <div>
                     <p className={styles.textPurple}>Completed</p>
-                    <span>1 of 10</span>
+                    <span>{completedTasks} of {tasksQuantity}</span>
                 </div>
             </header>
 
